407032579b98150100041848fb41478e422a1674
dolmetschen-cottbus <dolmetschen-cottbus@gmx.net>
2026-02-03 16:33:56 +0100

    Update lang-redirect.js

--- js/lang-redirect.js
+++ js/lang-redirect.js
@@ -2,18 +2,23 @@
     const supported = ["de", "en", "ru"];
     let storedLang = localStorage.getItem("lang");
 
-    if(storedLang && supported.includes(storedLang)){
+    if (storedLang && supported.includes(storedLang)) {
         window.location.replace(`/web/${storedLang}/`);
         return;
     }
 
-    const browserLang = navigator.language ? navigator.language.slice(0,2) : 'de';
+    // Alle bevorzugten Sprachen des Browsers prüfen
+    const browserLangs = navigator.languages || [navigator.language] || ['de'];
+    let lang = "de"; // default fallback
 
-    if(supported.includes(browserLang)){
-        localStorage.setItem("lang", browserLang);
-        window.location.replace(`/web/${browserLang}/`);
-    } else {
-        localStorage.setItem("lang", "de");
-        window.location.replace("/web/de/");
+    for (let l of browserLangs) {
+        l = l.slice(0,2).toLowerCase();
+        if (supported.includes(l)) {
+            lang = l;
+            break;
+        }
     }
+
+    localStorage.setItem("lang", lang);
+    window.location.replace(`/web/${lang}/`);
 })();
